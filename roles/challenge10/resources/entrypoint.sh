ee261dfa866e7865ffc32a43a4764f9e918ca54e
Elliot DeMatteis <redx47@gmail.com>
2019-09-10 16:52:14 -0700

    Making entrypoint script infinite.

--- roles/challenge10/resources/entrypoint.sh
+++ roles/challenge10/resources/entrypoint.sh
@@ -2,3 +2,4 @@ service ssh start
 PORT=$((1000 + RANDOM % 32000))
 su - webserver -c "python -m SimpleHTTPAuthServer $PORT --dir /home/webserver/"
 PORT=$((1000 + RANDOM % 32000))
+tail -f /dev/null
