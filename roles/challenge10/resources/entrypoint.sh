2fffa0c224b19516f04fe6c7d0041683a55748a2
Elliot DeMatteis <redx47@gmail.com>
2019-09-10 16:31:31 -0700

    challenge 10 doesnt need setcap, using high ports

--- roles/challenge10/resources/entrypoint.sh
+++ roles/challenge10/resources/entrypoint.sh
@@ -1,5 +1,4 @@
 service ssh start
-setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/python2.7
 PORT=$((1000 + RANDOM % 32000))
 su - webserver -c "python -m SimpleHTTPAuthServer $PORT --dir /home/webserver/"
 PORT=$((1000 + RANDOM % 32000))
