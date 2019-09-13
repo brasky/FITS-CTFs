98128fcd397a3a9660f87a979d2efb45ec459087
Elliot DeMatteis <redx47@gmail.com>
2019-09-13 11:03:25 -0700

    fixing permissions, installing setcap

--- roles/challenge11/resources/entrypoint.sh
+++ roles/challenge11/resources/entrypoint.sh
@@ -1,3 +1,5 @@
 setcap CAP_NET_BIND_SERVICE=+eip /bin/nc
 rm -f /tmp/f; mkfifo /tmp/f
+chown jack:jack /tmp/f
+chmod 644 /tmp/f
 su - jack -c "cat /tmp/f | /bin/sh -i 2>&1 | nc -l 0.0.0.0 22 > /tmp/f"
