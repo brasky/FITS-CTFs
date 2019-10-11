6832775ac651cb09b6e2ae8787d37845d1c37999
Elliot DeMatteis <redx47@gmail.com>
2019-10-10 17:19:40 -0700

    beginnerweb7 complete

--- roles/beginnerweb7/readme.md
+++ roles/beginnerweb7/readme.md
@@ -2,7 +2,7 @@
 
 ## Overview 
 
-Challenge is to browse to a website and use LFI to read the flag.
+Challenge is to browse to a website and use LFI to read the flag. There is a "file viewer" site set up that claims to only be able to read files from the local dir. In reality, it's only validating input in the html form, not in the php, so someone can enter test.txt, intercept the request, and change it to /home/jack/flag.txt or they can just craft a post request with that field.
 
 Apache is running on port 8007 in an ubuntu 18.04 docker container.
 
--- roles/beginnerweb7/resources/web_content/index.php
+++ roles/beginnerweb7/resources/web_content/index.php
@@ -8,12 +8,29 @@
 
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-    <title>Jack's Doc Viewer</title>
+    <title>Jack's File Viewer</title>
   </head>
   <body>
-    <?php
-    file_get_contents('/home/jack/flag.txt')
+  <div class="container">
+    <form action="" method="post" name="Login_Form" class="form-signin">
+        <h2 class="form-signin-heading">Enter the file you want to view!</h2>
+        <label for="inputFile" class="sr-only">File</label>
+        <input pattern="[A-Za-z0-9.]*" name="File" type="text" id="inputFile" class="form-control" placeholder="file" required autofocus>
+
+
+        <button name="Submit" value="Login" class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+        <!--Note: No special characters, slashes, etc are allowed to be entered! That way only files in the current directory can be viewed. -->
+        <?php
+        $file = $_POST['File'];
+
+    echo file_get_contents($_POST['File']);
     ?>
+
+    </form>
+
+</div>
+
+
 
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
